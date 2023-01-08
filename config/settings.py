be603270af2b3acc96ca77bf8c00d7d6c7956d57
Unekwu001 <unekwutheophilus@gmail.com>
2023-01-08 22:30:13 +0100

    add

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/enema/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/enema/static/')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
