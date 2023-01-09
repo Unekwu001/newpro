a8a0eee8fda3c66b7eebbff5927a863ea11e21e2
Unekwu001 <unekwutheophilus@gmail.com>
2023-01-09 12:40:20 +0100

    commit

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'unekwutheophilus@gmail.com'
 EMAIL_HOST_PASSWORD = 'nhichanaffghfdko'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'default from email'
 
